dcca95bf7e765ff54f05eed5bdaaf03f75d1f9d2
sxu2583 <shihabuddin100@gmail.com>
2017-01-25 20:05:56 -0500

    Image

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ class App extends Component {
         <Form inline>
           <FormControl
             className="Deadline-input"
-            placeholder="new Date"
+            placeholder="New Date"
             onChange={event => this.setState({newDeadline: event.target.value})}
           />
           <Button onClick={() => this.changeDeadline()}>
